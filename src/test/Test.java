c06af8528fdbbeb4be48ea184a6c6058d28d947e
Tanmay76 <65888184+Tanmay76@users.noreply.github.com>
2026-04-18 06:18:42 +0000

    uncomment premitive data type test method

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,13 +14,13 @@ public class Test {
 		
 		// premitive data type
 		//	  byte, int (32 bit) , char (16 bit), String, float(32 bit), boolean, double , short (16 bit)
-//		int [] arra = {1,2,3,4};
-//		int [] arrb = arra;
-//		// print before changes
-//		printArray(arra);
-//		System.out.println("After changing other refrence value: ");
-//		arrb[2] = 7;
-//		printArray(arra);	
+		int [] arra = {1,2,3,4};
+		int [] arrb = arra;
+		// print before changes
+		printArray(arra);
+		System.out.println("After changing other refrence value: ");
+		arrb[2] = 7;
+		printArray(arra);	
 		
 		// example of volatile keyword 
 //		WorkerThread workerThread = new WorkerThread();
